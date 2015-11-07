f60a3d727076f89345b7da08d6fff2040d796c02
sbelov <belov.stan@gmail.com>
2015-11-07 12:00:11 -0500

    First commit

---
+++ awesomeagile-webapp/src/main/java/org/awesomeagile/boot/AwesomeAgileApplication.java
@@ -0,0 +1,17 @@
+package org.awesomeagile.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+/**
+ * @author sbelov@google.com (Stan Belov)
+ */
+@SpringBootApplication
+@EnableConfigurationProperties
+public class AwesomeAgileApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AwesomeAgileApplication.class, args);
+  }
+}
