83be40e0c4a099bd39dfce35df45a28cc31f331d
sbelov <belov.stan@gmail.com>
2015-11-08 15:16:53 -0500

    Refactored the database test environment to be easier to use

---
+++ awesomeagile-dao/src/main/java/org/awesomeagile/data/test/TestDatabase.java
@@ -0,0 +1,182 @@
+package org.awesomeagile.data.test;
+
+import static java.util.Arrays.asList;
+import static org.junit.Assert.assertEquals;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+
+import com.spotify.docker.client.DefaultDockerClient;
+import com.spotify.docker.client.messages.ContainerConfig;
+import com.spotify.docker.client.messages.ContainerCreation;
+import com.spotify.docker.client.messages.HostConfig;
+import com.spotify.docker.client.messages.PortBinding;
+
+import org.apache.commons.lang.math.RandomUtils;
+import org.junit.Test;
+import org.junit.rules.ExternalResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.ConnectionCallback;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author sbelov@google.com (Stan Belov)
+ */
+public class TestDatabase extends ExternalResource {
+
+  public static final int TIMEOUT = 5;
+  public static final int RETRIES = 30;
+  public static final int POSTGRESQL_PORT = 5432;
+  public static final String POSTGRES_9_2_IMAGE = "postgres:9.4";
+  private String hostName;
+  private ContainerCreation container;
+  private DefaultDockerClient docker;
+  private int localPort;
+  private final String databaseName;
+  private final List<String> scripts;
+
+  public TestDatabase(String databaseName, String... scripts) {
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(databaseName));
+    this.databaseName = databaseName;
+    this.scripts = asList(scripts);
+  }
+
+  @Override
+  protected void before() throws Throwable {
+    // Create a client based on DOCKER_HOST and DOCKER_CERT_PATH env vars
+    docker = DefaultDockerClient.fromEnv().build();
+    hostName = docker.getHost();
+    // Bind container ports to host ports
+    localPort = findAvailablePort();
+    final HostConfig hostConfig = HostConfig.builder()
+        .portBindings(createPortBinding(POSTGRESQL_PORT, localPort))
+        .build();
+    docker.pull(POSTGRES_9_2_IMAGE);
+    this.container = docker.createContainer(ContainerConfig.builder()
+        .image(POSTGRES_9_2_IMAGE)
+        .hostConfig(hostConfig)
+        .exposedPorts(String.valueOf(POSTGRESQL_PORT))
+        .build());
+    docker.startContainer(container.id());
+    waitForDatabase(RETRIES);
+    createDatabase();
+    for (String script : scripts) {
+      executeScript(script);
+    }
+  }
+
+  public void executeScript(final String script) {
+    executeScript(Optional.of(databaseName), script);
+  }
+
+  @Test
+  public void testPostgres() throws Exception {
+    JdbcTemplate jdbcTemplate = jdbcTemplate();
+    int random = RandomUtils.nextInt(1000);
+    Integer integer = jdbcTemplate.queryForObject("select " + random, Integer.class);
+    assertEquals(random, integer.intValue());
+    System.out.printf("select %d returns: %d\n", random, integer);
+  }
+
+  public JdbcTemplate jdbcTemplate() {
+    return new JdbcTemplate(
+        new DriverManagerDataSource(
+            /* url */ getUrl(),
+            /* username */ "postgres",
+            /* password */ ""));
+  }
+
+  public JdbcTemplate jdbcTemplate(String database) {
+    return new JdbcTemplate(
+        new DriverManagerDataSource(
+            /* url */ getUrl(database),
+            /* username */ "postgres",
+            /* password */ ""));
+  }
+
+  public String getUrl() {
+    return String.format("jdbc:postgresql://%s:%d/", hostName, localPort);
+  }
+
+  public String getUrl(String database) {
+    return String.format("jdbc:postgresql://%s:%d/%s", hostName, localPort, database);
+  }
+
+  @Override
+  protected void after() {
+    try {
+      docker.stopContainer(container.id(), TIMEOUT);
+      docker.removeContainer(container.id());
+    } catch (Exception ex) {
+      throw new RuntimeException("Unable to stop the Docker container", ex);
+    }
+  }
+
+  private static Map<String, List<PortBinding>> createPortBinding(int from, int to) {
+    final Map<String, List<PortBinding>> portBindings = new HashMap<>();
+    List<PortBinding> hostPorts = new ArrayList<>();
+    hostPorts.add(PortBinding.of("0.0.0.0", to));
+    portBindings.put(String.valueOf(from), hostPorts);
+    return portBindings;
+  }
+
+  private int findAvailablePort() throws IOException {
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(0);
+      return serverSocket.getLocalPort();
+    } finally {
+      if (serverSocket != null) {
+        serverSocket.close();
+      }
+    }
+  }
+
+  private void waitForDatabase(int retries) {
+    JdbcTemplate jdbcTemplate = jdbcTemplate();
+    int left = retries;
+    while (left > 0) {
+      left--;
+      System.out.println(left + " attempts left");
+      try {
+        Thread.sleep(1000);
+        jdbcTemplate.execute("select 1");
+        return;
+      } catch (Exception ex) {
+        // ignore
+      }
+    }
+    throw new RuntimeException("Database did not come up after " + retries + " attempts");
+  }
+
+  private void executeScript(Optional<String> databaseName, final String script) {
+    JdbcTemplate jdbcTemplate =
+        databaseName.isPresent() ? jdbcTemplate(databaseName.get()) : jdbcTemplate();
+    jdbcTemplate.execute(new ConnectionCallback<Object>() {
+      @Override
+      public Object doInConnection(Connection con) throws SQLException, DataAccessException {
+        ScriptUtils.executeSqlScript(con, new ClassPathResource(script));
+        return null;
+      }
+    });
+  }
+
+  private void createDatabase() {
+    JdbcTemplate jdbcTemplate = jdbcTemplate();
+    jdbcTemplate.execute(String.format("DROP DATABASE IF EXISTS %s", databaseName));
+    jdbcTemplate.execute(String.format("CREATE DATABASE %s", databaseName));
+  }
+}
--- awesomeagile-webapp/src/test/java/org/awesomeagile/webapp/test/DatabaseIntegrationTest.java
+++ awesomeagile-webapp/src/test/java/org/awesomeagile/webapp/test/DatabaseIntegrationTest.java
@@ -1,26 +1,19 @@
 package org.awesomeagile.webapp.test;
 
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.junit.Assert.assertEquals;
-
-import com.spotify.docker.client.DefaultDockerClient;
-import com.spotify.docker.client.messages.ContainerConfig;
-import com.spotify.docker.client.messages.ContainerCreation;
-import com.spotify.docker.client.messages.HostConfig;
-import com.spotify.docker.client.messages.PortBinding;
+import static org.junit.Assert.assertThat;
 
 import org.apache.commons.lang.math.RandomUtils;
-import org.junit.After;
-import org.junit.Before;
+import org.awesomeagile.data.test.TestDatabase;
+import org.awesomeagile.model.team.User;
+import org.hamcrest.CustomMatcher;
+import org.junit.ClassRule;
 import org.junit.Test;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * A test that brings up a local PostgreSQL instance within a Docker container.
@@ -30,97 +23,43 @@ import java.util.Map;
  */
 public class DatabaseIntegrationTest {
 
-  public static final int TIMEOUT = 5;
-  public static final int RETRIES = 30;
-  public static final int POSTGRESQL_PORT = 5432;
-  public static final String POSTGRES_9_2_IMAGE = "postgres:9.4";
-  private String hostName;
-  private ContainerCreation container;
-  private DefaultDockerClient docker;
-  private int localPort;
+  private static final String DATABASE_NAME = "awesomeagile";
 
-  @Before
-  public void setUp() throws Exception {
-    // Create a client based on DOCKER_HOST and DOCKER_CERT_PATH env vars
-    docker = DefaultDockerClient.fromEnv().build();
-    hostName = docker.getHost();
-    // Bind container ports to host ports
-    localPort = findAvailablePort();
-    final HostConfig hostConfig = HostConfig.builder()
-        .portBindings(createPortBinding(POSTGRESQL_PORT, localPort))
-        .build();
-    docker.pull(POSTGRES_9_2_IMAGE);
-    this.container = docker.createContainer(ContainerConfig.builder()
-        .image(POSTGRES_9_2_IMAGE)
-        .hostConfig(hostConfig)
-        .exposedPorts(String.valueOf(POSTGRESQL_PORT))
-        .build());
-    docker.startContainer(container.id());
-    waitForDatabase(RETRIES);
-    System.out.println("PostgreSQL server available at " + getUrl());
-  }
-
-  @After
-  public void tearDown() throws Exception {
-    docker.stopContainer(container.id(), TIMEOUT);
-    docker.removeContainer(container.id());
-  }
-
-  private static Map<String, List<PortBinding>> createPortBinding(int from, int to) {
-    final Map<String, List<PortBinding>> portBindings = new HashMap<>();
-    List<PortBinding> hostPorts = new ArrayList<>();
-    hostPorts.add(PortBinding.of("0.0.0.0", to));
-    portBindings.put(String.valueOf(from), hostPorts);
-    return portBindings;
-  }
+  @ClassRule
+  public static TestDatabase testDatabase = new TestDatabase(
+      DATABASE_NAME,
+      "/create-schema.sql",
+      "/create-sample-data.sql"
+  );
 
   @Test
   public void testPostgres() throws Exception {
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(
-        new DriverManagerDataSource(
-            /* url */ getUrl(),
-            /* username */ "postgres",
-            /* password */ ""));
+    JdbcTemplate jdbcTemplate = testDatabase.jdbcTemplate();
     int random = RandomUtils.nextInt(1000);
     Integer integer = jdbcTemplate.queryForObject("select " + random, Integer.class);
     assertEquals(random, integer.intValue());
-    System.out.printf("select %d returns: %d\n", random, integer);
   }
 
-  private void waitForDatabase(int retries) {
-    JdbcTemplate jdbcTemplate = new JdbcTemplate(
-        new DriverManagerDataSource(
-            /* url */ getUrl(),
-            /* username */ "postgres",
-            /* password */ ""));
-    int left = retries;
-    while (left > 0) {
-      left--;
-      System.out.println(left + " attempts left");
-      try {
-        Thread.sleep(1000);
-        jdbcTemplate.execute("select 1");
-        return;
-      } catch (Exception ex) {
-        // ignore
-      }
+  @Test
+  public void testUsers() throws Exception {
+    JdbcTemplate jdbcTemplate = testDatabase.jdbcTemplate(DATABASE_NAME);
+    List<User> users = jdbcTemplate
+        .query("select * from teams.user", new BeanPropertyRowMapper<>(User.class));
+    assertEquals(2, users.size());
+    assertThat(users, hasItem(new UserWithName("stan")));
+  }
+
+  private static final class UserWithName extends CustomMatcher<User> {
+    private final String name;
+
+    public UserWithName(String name) {
+      super("user with name " + name);
+      this.name = name;
     }
-    throw new RuntimeException("Database did not come up after " + retries + " attempts");
-  }
 
-  private int findAvailablePort() throws IOException {
-    ServerSocket serverSocket = null;
-    try {
-      serverSocket = new ServerSocket(0);
-      return serverSocket.getLocalPort();
-    } finally {
-      if (serverSocket != null) {
-        serverSocket.close();
-      }
+    @Override
+    public boolean matches(Object item) {
+      return item instanceof User && name.equals(((User) item).getDisplayName());
     }
-  }
-
-  private String getUrl() {
-    return String.format("jdbc:postgresql://%s:%d/", hostName, localPort);
   }
 }
