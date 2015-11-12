b93ac1105e3264057c050834869ce06286cce0ca
Phillip Heller <phheller@cisco.com>
2015-11-11 20:33:47 -0500

    Corrected environment variables in travis

--- awesomeagile-webapp/src/scripts/deploy-test.sh
+++ awesomeagile-webapp/src/scripts/deploy-test.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-# set DOCKERHUB_USERNAME, DOCKERHUB_PASSWORD, and DOCKERHUB_EMAIL
+# set DOCKER_HUB_USERNAME, DOCKER_HUB_PASSWORD, and DOCKER_HUB_EMAIL
+
+mvn package -DskipTests
 
 mvn -pl org.awesomeagile:awesomeagile-webapp \
   -DskipTests \
-  -Dsettings.security=awesomeagile-webapp/src/scripts/maven_security_settings.xml \
   --settings awesomeagile-webapp/src/scripts/maven_settings.xml \
-  package docker:build docker:push
+  docker:build docker:push
 
 # set AWS_ACCESS_KEY and AWS_SECRET_ACCESS_KEY
 
